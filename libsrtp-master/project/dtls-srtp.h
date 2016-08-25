7d8a24832d3182ce31cde9704635227bf8764173
tariromukute <tariromukute@gmail.com>
2016-08-25 13:46:36 +0200

    Added on to the proccess flow commenting for dtls_srtp

--- libsrtp-master/project/dtls-srtp.h
+++ libsrtp-master/project/dtls-srtp.h
@@ -16,14 +16,13 @@ uint8 srtp_protection_profile[2];
 
 /**
  * use_srtp_data must be put into the extension_data field of the "use_srtp"
+ * rtp_mki value contains the SRTP Master Key
+ * Identifier (MKI) value (if any) that the client will use for his SRTP
+ * packets.  If this field is of zero length, then no MKI will be used.
+ * A session uses the same mki
  */
 struct {
   srtp_protection_profiles srtp_rotection_profiles;
-  /**
-   * rtp_mki value contains the SRTP Master Key
-   * Identifier (MKI) value (if any) that the client will use for his SRTP
-   * packets.  If this field is of zero length, then no MKI will be used
-   */
   opaque srtp_mki; //<0..255> variable length, maybe 0 to 255 bytes
 } use_srtp_data;
 
@@ -32,6 +31,40 @@ struct {
  * in descending order
  */
 srtp_protection_profile srtp_rotection_profiles<2..2^16-1>;
+
+
+//======================================================================================================
+
+/**
+ * Called when a packet is received.
+ * It then calls other implementation methods
+ */
+void dtls_srtp_on_reception();
+
+/** Packet Check Function **
+ * 
+ *                  +----------------+
+ *                  | 127 < B < 192 -+--> forward to RTP
+ *                  |                |
+ *      packet -->  |  19 < B < 64  -+--> forward to DTLS
+ *                  |                |
+ *                  |       B < 2   -+--> forward to STUN
+ *                  +----------------+
+ * Can optionally be put in the driver file
+ * Must be called to determine what process to take, whether its handshaking or not
+ */
+void dtls_srtp_packet_type();
+
+/**
+ * Algorithm to tell apart two DTLS_SRTP packets that will be
+ * from different users but using the same port, distinguition is
+ * by SSRC, see https://tools.ietf.org/html/rfc5764#page-5
+ * method to be implemented last as during test of the project 
+ * doesn't impact
+ */
+void dtls_srtp_packet_distingution();
+
+
 
 
 /** step 1 **
@@ -90,5 +123,24 @@ void dtls_srtp_send_find();
 
 /* step 6 */ 
 void dtls_srtp_receive_find();
+
+/**
+ * When this limit is reached, a new DTLS session SHOULD be used
+ * to establish replacement keys, and SRTP implementations MUST NOT use
+ * the existing keys for the processing of either outbound or inbound
+ * traffic
+ */
+void dtls_srtp_on_limit();
+
+/**
+ * Rekeying in DTLS is accomplished by performing a new handshake over
+ * the existing DTLS channel
+ * the handshake messages are protected by the existing DTLS cipher suite
+ * handshake can be parallel with data transfer
+ * new sets of keys used once handshake done. the old and new should be
+ * stored for a while because of packet reordering, advisably for 
+ * duration of the maximum segment lifetime (MSL)
+ */
+void dtls_srtp_rehandshake();
 
 #endif
